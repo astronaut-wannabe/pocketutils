0c66a6cae89caa4d576460b184be881c9a2d147f
astronaut-wannabe <contact@astronaut-wannabe.com>
2014-09-13 20:58:54 -0700

    Add ViewHolder to ViewFlipperCursorAdapter

--- app/src/main/java/com/astronaut_wannabe/pocketutil/ViewFlipperCursorAdapter.java
+++ app/src/main/java/com/astronaut_wannabe/pocketutil/ViewFlipperCursorAdapter.java
@@ -12,7 +12,9 @@ import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.astronaut_wannabe.pocketutil.data.PocketDataContract;
 
@@ -20,6 +22,8 @@ import com.astronaut_wannabe.pocketutil.data.PocketDataContract;
  * Created by ***REMOVED*** on 9/13/14.
  */
 public class ViewFlipperCursorAdapter extends CursorAdapter implements LoaderManager.LoaderCallbacks <Cursor>{
+    private static final String LOG_TAG = ViewFlipperCursorAdapter.class.getSimpleName();
+
     private final static String[] POCKET_COLUMNS = {
             PocketDataContract.PocketItemEntry.TABLE_NAME + "." + PocketDataContract.PocketItemEntry._ID,
             PocketDataContract.PocketItemEntry.COLUMN_TITLE,
@@ -34,27 +38,37 @@ public class ViewFlipperCursorAdapter extends CursorAdapter implements LoaderMan
     public final static int COL_EXCERPT  = 2;
     public final static int COL_POCKET_ITEM_ID = 3;
     public final static int COL_POCKET_URL = 4;
-    private static final String LOG_TAG = ViewFlipperCursorAdapter.class.getSimpleName();
 
+    private final View.OnClickListener mButtonPlaceholderClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Toast.makeText(v.getContext(),"Button Clicked:"+v.getId(),Toast.LENGTH_SHORT).show();
+        }
+    };
     public ViewFlipperCursorAdapter(Context context, Cursor c, int flags) {
         super(context, c, ViewFlipperCursorAdapter.FLAG_REGISTER_CONTENT_OBSERVER);
     }
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return LayoutInflater.from(context).inflate(R.layout.list_item_pocket,parent,false);
+        final View view = LayoutInflater.from(context).inflate(R.layout.list_item_pocket,parent,false);
+        final ViewHolder vh = new ViewHolder(view);
+        view.setTag(vh);
+        return view;
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        final TextView titleTextView = (TextView) view.findViewById(R.id.article_title);
-        final TextView excerptTextView = (TextView) view.findViewById(R.id.article_excerpt);
-
+        final ViewHolder vh = (ViewHolder) view.getTag();
         final String title = cursor.getString(COL_TITLE);
         final String excerpt = cursor.getString(COL_EXCERPT);
+        final String articleId = cursor.getString(COL_POCKET_ITEM_ID);
         final String htmlString = mContext.getString(R.string.title_and_synopsis_format, title);
-        titleTextView.setText(Html.fromHtml(htmlString));
-        excerptTextView.setText(excerpt);
+        vh.title.setText(Html.fromHtml(htmlString));
+        vh.excerpt.setText(excerpt);
+        vh.id.setText(articleId);
+        vh.delete_btn.setOnClickListener(mButtonPlaceholderClickListener);
+        vh.save_btn.setOnClickListener(mButtonPlaceholderClickListener);
     }
 
     @Override
@@ -81,5 +95,18 @@ public class ViewFlipperCursorAdapter extends CursorAdapter implements LoaderMan
     @Override
     public void onLoaderReset(Loader loader) {
         swapCursor(null);
+    }
+
+    private static class ViewHolder {
+        public final TextView title, excerpt,id;
+        public final Button delete_btn, save_btn;
+
+        public ViewHolder(View view){
+            title = (TextView) view.findViewById(R.id.article_title);
+            excerpt = (TextView) view.findViewById(R.id.article_excerpt);
+            id = (TextView) view.findViewById(R.id.article_id);
+            delete_btn = (Button) view.findViewById(R.id.article_delete_btn);
+            save_btn = (Button) view.findViewById(R.id.article_save_btn);
+        }
     }
 }
